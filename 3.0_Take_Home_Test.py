b37060232ab41a01c272cd02abe22e4fe47b2ade
agicedd <agicedd@urbandaleschools.com>
2018-12-18 13:37:00 -0600

    Deleted previous take home test and replaced it with updated one.  Did some questions.

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -1,135 +1,72 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
 but I will definitely not ask another person except the instructor. Signed: Eddie H. Agic
-
-
-
 1. Write a line of code that will print your name.
 '''
 
-print("Eddie Agic")
+print("Eddie")
 
 '''
-2. How do you enter a comment in a program?
-'''
-#Like this, or with the three apostraphe marks.  Also why is this underlined it is very annoying.
-
-'''
-3. What is the ouput of the following lines of code?
-print (2/3)
-print (2//3)
+2. Write a program that asks someone for their name and then prints their name to the screen?
 '''
 
-#Runs the equations... .666 repeating and 0 respectivley.
+name = input("Your Name: ")
+print(name)
 
 '''
-4. Write a line of code that creates a variable called pi and sets it to an
-appropriate value.
+3. Predict the output of the following lines of code and then test them? Write down your prediction and the output.
+print (17/9) - PREDICTION: Will do division
+print (17//9) - PREDICTION: Will floor
+print (17%9) - PREDICTION: Will modulus
 '''
 
+print(17/9)
+print(17//9)
+print(17%9)
+
+#I was correct on all of them. =)
+
+'''
+4. Write a a program where a user enters a base and height and you print the area of a triangle.
+'''
+
+height=int(input("Enter Height: "))
+base=int(input("Enter Base: "))
 print()
 
 '''
-5. Why does this code not work?
-A=22
+5. Change this program so it works.
+A=May the Force be with you!
 print(a)
 '''
 
 
 
+
 '''
-6. All of the variable names below can be used. But which of these is the best?
-a
-A
-Area
-AREA
-area
-areaOfRectangle
-AreaOfRectangle
+6. Write a line of code that will ask the user for the length of a square's
+side and then print the area of the square
 '''
 
 
-'''
-7. Which of these variables names are not allowed in Python?
-apple
-Apple
-APPLE
-Apple2
-1Apple
-account number
-account_number
-account.number
-accountNumber
-account#
+
+'''7. Ask a user for the length of radii of an ellipse and then print its area. 
+Area=pi*a*b where a and b are the lengths of the major radii.
 '''
 
 
 
 '''
-8. Why does this code not work?
-
-print(a)
-a=45
-'''
-
-
-
-'''
-9. Write a line of code that will ask the user for the length of a square's
-side and store the result in a variable. Make sure to convert the value
-to an integer.
-'''
-
-
-'''
-10. Write a line of code that prints the area of the square, using the
-number the user typed in that you stored in question 9.
-'''
-
-
-
-'''11. Do the same as in questions 9 and 10, but with the formula for the
-area of an ellipse. Area=pi*a*b where a and b are the lengths of the major radii.
-'''
-
-
-
-'''
-12. Do the same as in questions 9 and 10, but with a formula to find
-the pressure of a gas. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
+8. Ask a user for moles, volume and temperature of a gas and print out the pressure. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
 volume, and R is the gas constant 8.3144.
 '''
 
-
 '''
-13. Explain the mistake in this code:
-
-pi = float(3.14)
-'''
-
-
-
-'''
-14. Assume radius has been defined. This code runs, but isn't very good. Explain the mistake in the following code:
-
-x=3.14
-pi=x
-area=pi*radius**2
+9. Ask a user for an integer and then print the square root.
 '''
 
 
 '''
-Assuming x and y are already defined, this code runs. But
-something isn't quite right. Explain the mistake in the following code:
-
-a=((x)*(y))
+10. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. Ask the user for mass and acceleration
+and then print out the Force on one line and "Get it?" on the next.
 '''
-
-
-'''
-16. Explain the mistake in the following code:
-
-radius = input(float("Enter the radius:"))
-'''
-
-
